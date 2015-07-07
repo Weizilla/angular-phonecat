e5a9c0e391e8954fc9616a31fc48778cfa055e1d
Wei Yang <wei.13.yang@here.com>
2015-07-07 13:30:44 -0700

    step 4

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,13 +1,18 @@
 describe("PhoneCat App", function() {
     describe("Phone list view", function() {
+        var phoneList;
+        var query;
+
         beforeEach(function() {
             browser.get("app/index.html");
         });
 
-        var phoneList = element.all(by.repeater("phone in ctrl.phones"));
-        var query = element(by.model("query"));
+        beforeEach(function() {
+            phoneList = element.all(by.repeater("phone in ctrl.phones"));
+            query = element(by.model("ctrl.query"));
+        });
 
-        it("should filter the phoen list as a user types into the search box", function() {
+        it("should filter the phone list as a user types into the search box", function() {
             expect(phoneList.count()).toBe(3);
 
             query.sendKeys("nexus");
@@ -25,5 +30,33 @@ describe("PhoneCat App", function() {
             query.sendKeys("nexus");
             expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
         });
-    })
+
+        it("should be possible to control phone order via drop down select box", function() {
+            // space required for search text in column.() because tag in index.html has space:
+            // <span>{{ phone.name }}</span>. Seems to be fixed in later version where spaces do
+            // not matter
+            var phoneNameColumn =
+                element.all(by.repeater("phone in ctrl.phones").column(" phone.name "));
+
+            function getNames() {
+                return phoneNameColumn.map(function(e) {
+                    return e.getText();
+                });
+            }
+
+            expect(phoneNameColumn.count()).toBe(3);
+
+            query.sendKeys("tablet");
+            expect(getNames()).toEqual([
+                "Motorola XOOM\u2122 with Wi-Fi",
+                "MOTOROLA XOOM\u2122"
+            ]);
+
+            element(by.model("ctrl.orderProp")).element(by.css("option[value='name']")).click();
+            expect(getNames()).toEqual([
+                "MOTOROLA XOOM\u2122",
+                "Motorola XOOM\u2122 with Wi-Fi"
+            ]);
+        });
+    });
 });
